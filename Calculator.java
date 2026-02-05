82585e87ddb68cf3712c32193285da80e362ebee
sowjanya-it <159770816+sowjanya-it@users.noreply.github.com>
2026-02-05 05:57:31 +0000

    calculator modified

--- Calculator.java
+++ Calculator.java
@@ -8,9 +8,9 @@ int r1 = n1+n2;
 int r2 = n1-n2;
 int r3 = n1*n2;
 int r4 = n1/n2;
-System.out.println(“Addition: “+r1);
-System.out.println(“Subtraction: “+r2);
-System.out.println(“Multiplication: “+r3);
-System.out.println(“Division: “+r4);
+System.out.println("Addition: "+r1);
+System.out.println("Subtraction: "+r2);
+System.out.println("Multiplication: "+r3);
+System.out.println("Division: "+r4);
 }
 }
